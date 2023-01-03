033cd638f5b1cf00cfea9408210e7923947a8372
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-01-03 16:40:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import { setItem } from "./utils/storage";
 import "./index.scss";
 import "bootstrap";
 
-
 document.getElementById("btnClick").addEventListener("click", () => {
+
     alert("Hello Webpack");
-    setItem(
-        "token",
-        "klsdjfklsjkdfjksldsdkjfklsjdfklsjdlfkjslkdfjklsfklsjkldfjskljfklsjfl"
-    );
+    alert("v1");
+    alert("v2");
+    alert("v3");
+    setItem("token", "dfskldjfkdfldlşsdgsdkgflskşdfşkdfnlknsvkndsşfkşdsfdsfjksşş");
 });
+
